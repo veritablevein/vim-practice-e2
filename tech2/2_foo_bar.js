17543a71bff47d0d41a86381a9484cd0d6b40ee1
veritablevein <veritablevein@gmail.com>
2020-10-30 19:38:05 +0800

    减少无关移动：A代替$a

--- tech2/2_foo_bar.js
+++ tech2/2_foo_bar.js
@@ -1,3 +1,7 @@
-var foo = 1
-var bar = 'a'
-var foobar = foo + bar
+var foo = 1;
+var bar = 'a';
+var foobar = foo + bar;
+// A;<ESC>      $a;<ESC>
+// j.           j$.
+// j.           j$.
+// (光标移到行尾+";"；a光标后插入；i光标前插入；$移动到行尾；A移动到行尾并插入=$a)
